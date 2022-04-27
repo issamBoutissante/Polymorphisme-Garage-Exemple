ae25425fad4d9e9a05753b5d9f65318328a1206b
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2022-04-27 13:38:57 +0200

    La Supprission A ete Ajouter

--- Garage.cpp
+++ Garage.cpp
@@ -1,7 +1,11 @@
 #include "Garage.h"
+#include<iostream>
+using namespace std;
 void Garage::ajouter(Vehicule* vehicule) {
 	this->Vehicules.push_back(vehicule);
+	cout << "- Nombre Vehicule : " << this->Vehicules.size() << endl;
 }
 void Garage::supprimer(int index) {
-	delete this->Vehicules[index];
+	this->Vehicules.erase(this->Vehicules.begin() + index);
+	cout << "- Nombre Vehicule : " << this->Vehicules.size() << endl;
 }
--- Main.cpp
+++ Main.cpp
@@ -11,11 +11,10 @@ int main()
 	garage.ajouter(new Moto(3, 2000, 2, 90));
 	garage.ajouter(new Voiture(4, 1999, 7, 40000));
 	garage.ajouter(new Camion(4,2022,8,50));
-	cout << "-     Moto : \n\n" << endl;
+	garage.supprimer(1);
+
 	garage[0]->afficher();
-	cout << "-     Voiture : \n\n" << endl;
 	garage[1]->afficher();
-	cout << "-     Camion : \n\n" << endl;
-	garage[2]->afficher();
+
 	return 0;
 }
